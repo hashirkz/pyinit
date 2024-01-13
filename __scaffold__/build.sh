ab023df6a796fdc7bff46395d414621cbc2c2701
hashir <75959275+zzsleepyzzz@users.noreply.github.com>
2024-01-13 10:44:08 -0500

    bad glob pattern build.sh

--- __scaffold__/build.sh
+++ __scaffold__/build.sh
@@ -10,9 +10,7 @@ if [[ -d "dist" ]]; then
     rm -rf dist
 fi
 
-if [[ -d "*.egg-info" ]]; then
-    rm -rf *.egg-info
-fi
+rm -rf *.egg-info
 
 read -p "build wheel (yn)?" yn
 if [[ $yn == "y" || $yn == "Y" ]]; then
